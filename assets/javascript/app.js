29e210a0f1735c1a2ab2198ee1364ee0efa482e5
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-01-24 16:38:12 -0500

    All new portfolio NOW

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+$("#gh").on("click",function(){
+    window.open("https://github.com/jvallexm/")
+});
+$("#lin").on("click",function(){
+   window.open("https://www.linkedin.com/in/jennifer-valle/");
+});
